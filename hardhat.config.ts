7c4c4e32ac6ac3aa8f34e8beb0e8b61800e7bbbb
Jérémy James Toussaint <33313130+jeremyjams@users.noreply.github.com>
2024-03-07 14:44:23 +0100

    Prettify other .ts files

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,11 @@
 // SPDX-FileCopyrightText: 2024 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
 
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-toolbox';
+import { HardhatUserConfig } from 'hardhat/config';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+    solidity: '0.8.24',
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,16 +1,16 @@
 // SPDX-FileCopyrightText: 2024 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
 
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
 
 async function main() {
-  const voucherHub = await ethers.deployContract("VoucherHub");
-  await voucherHub.waitForDeployment();
+    const voucherHub = await ethers.deployContract('VoucherHub');
+    await voucherHub.waitForDeployment();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
