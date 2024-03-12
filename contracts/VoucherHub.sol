5689255f5c4f2bbd8ad89a5b3f5843f9db1bf066
Jérémy James Toussaint <33313130+jeremyjams@users.noreply.github.com>
2024-03-12 17:52:53 +0100

    Add upgradeable VoucherHub contract(s)

--- contracts/VoucherHub.sol
+++ contracts/VoucherHub.sol
@@ -1,14 +1,48 @@
 // SPDX-FileCopyrightText: 2024 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
 // SPDX-License-Identifier: Apache-2.0
 
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+import {IVoucherHub} from "./IVoucherHub.sol";
+
 pragma solidity ^0.8.20;
 
-contract VoucherHub {
-    event VoucherCreated();
+contract VoucherHub is OwnableUpgradeable, UUPSUpgradeable, IVoucherHub {
+    /// @custom:storage-location erc7201:iexec.storage.VoucherHub
+    struct VoucherHubStorage {
+        address _iexecAddress;
+    }
 
-    constructor() {}
+    // keccak256(abi.encode(uint256(keccak256("iexec.storage.VoucherHub")) - 1)) & ~bytes32(uint256(0xff));
+    bytes32 private constant VOUCHER_HUB_STORAGE_LOCATION =
+        0x8610b975c8d15083165a17dde673f1051edf836f87d56f9b9697dc45474fe600;
+
+    function _getVoucherHubStorage() private pure returns (VoucherHubStorage storage $) {
+        assembly {
+            $.slot := VOUCHER_HUB_STORAGE_LOCATION
+        }
+    }
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address iexecAddress) public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+        _getVoucherHubStorage()._iexecAddress = iexecAddress;
+    }
+
+    function getIexecAddress() public view returns (address) {
+        VoucherHubStorage storage $ = _getVoucherHubStorage();
+        return $._iexecAddress;
+    }
 
     function createVoucher() public {
         emit VoucherCreated();
     }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 }
