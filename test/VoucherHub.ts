d81903a737787b498b8f7dbbe490c45fd1d5e82a
Jérémy James Toussaint <33313130+jeremyjams@users.noreply.github.com>
2024-03-12 17:55:30 +0100

    Update VoucherHub tests

--- test/VoucherHub.ts
+++ test/VoucherHub.ts
@@ -3,7 +3,11 @@
 
 import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers';
 import { expect } from 'chai';
-import { ethers } from 'hardhat';
+import { ethers, upgrades } from 'hardhat';
+import { VoucherHub } from '../typechain-types/contracts';
+import { VoucherHubV2Mock } from '../typechain-types/contracts/mocks';
+
+const iexecAddress = '0x123456789a123456789b123456789b123456789d'; // random
 
 describe('VoucherHub', function () {
     // We define a fixture to reuse the same setup in every test.
@@ -14,10 +18,39 @@ describe('VoucherHub', function () {
         const [owner, otherAccount] = await ethers.getSigners();
 
         const VoucherHub = await ethers.getContractFactory('VoucherHub');
-        const voucherHub = await VoucherHub.deploy();
+        /**
+         * @dev Type declaration produces a warning until feature is supported by
+         * openzeppelin plugin. See "Support TypeChain in deployProxy function":
+         * https://github.com/OpenZeppelin/openzeppelin-upgrades/pull/535
+         */
+        const voucherHub: VoucherHub = await upgrades.deployProxy(VoucherHub, [iexecAddress]);
+        await voucherHub.waitForDeployment();
 
         return { voucherHub, owner, otherAccount };
     }
+
+    it('Should initialize', async () => {
+        const { voucherHub, owner } = await loadFixture(deployFixture);
+
+        expect(await voucherHub.owner()).to.equal(owner);
+        expect(await voucherHub.getIexecAddress()).to.equal(iexecAddress);
+    });
+
+    it('Should upgrade', async () => {
+        const { voucherHub } = await loadFixture(deployFixture);
+        const voucherHubAddress = await voucherHub.getAddress();
+        const VoucherHubV2 = await ethers.getContractFactory('VoucherHubV2Mock');
+        // Next line should throw if new storage schema is not compatible with previous one
+        const voucherHubV2: VoucherHubV2Mock = await upgrades.upgradeProxy(
+            voucherHubAddress,
+            VoucherHubV2,
+        );
+        await voucherHubV2.initializeV2('bar');
+
+        expect(await voucherHubV2.getAddress()).to.equal(voucherHubAddress);
+        expect(await voucherHubV2.getIexecAddress()).to.equal(iexecAddress); // V1
+        expect(await voucherHubV2.foo()).to.equal('bar'); // V2
+    });
 
     describe('VoucherCreated', function () {
         describe('Events', function () {
