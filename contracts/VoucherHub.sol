32cf01b2d5c377bdf27bc591601165be5173ef80
Gabriel Fournier <gabriel.fournier@iex.ec>
2024-03-14 16:20:28 +0100

    start adding voucher type and duration

--- contracts/VoucherHub.sol
+++ contracts/VoucherHub.sol
@@ -12,7 +12,19 @@ contract VoucherHub is OwnableUpgradeable, UUPSUpgradeable, IVoucherHub {
     /// @custom:storage-location erc7201:iexec.storage.VoucherHub
     struct VoucherHubStorage {
         address _iexecAddress;
+        VoucherTypeDescription[] _voucherTypeDescriptions;
+        mapping(uint256 => uint256) _voucherDurationByVoucherTypeId;
+        mapping(uint256 => mapping(address => bool)) _isAssetEligibleToMatchOrdersSponsoringByVoucherTypeId;
     }
+
+    struct VoucherTypeDescription {
+        uint256 voucherTypeId;
+        string description;
+    }
+
+    event VoucherTypeAdded(uint256 indexed voucherTypeId, string description);
+    event VoucherDurationSet(uint256 indexed voucherTypeId, uint256 duration);
+    event AssetEligibilitySet(uint256 indexed voucherTypeId, address asset, bool isEligible);
 
     // keccak256(abi.encode(uint256(keccak256("iexec.storage.VoucherHub")) - 1)) & ~bytes32(uint256(0xff));
     bytes32 private constant VOUCHER_HUB_STORAGE_LOCATION =
@@ -33,6 +45,31 @@ contract VoucherHub is OwnableUpgradeable, UUPSUpgradeable, IVoucherHub {
         __Ownable_init(msg.sender);
         __UUPSUpgradeable_init();
         _getVoucherHubStorage()._iexecAddress = iexecAddress;
+    }
+
+    function addVoucherTypeDescription(
+        uint256 voucherTypeId,
+        string memory description
+    ) public onlyOwner {
+        VoucherHubStorage storage $ = _getVoucherHubStorage();
+        $._voucherTypeDescriptions.push(VoucherTypeDescription(voucherTypeId, description));
+        emit VoucherTypeAdded(voucherTypeId, description);
+    }
+
+    function setVoucherDuration(uint256 voucherTypeId, uint256 duration) public onlyOwner {
+        VoucherHubStorage storage $ = _getVoucherHubStorage();
+        $._voucherDurationByVoucherTypeId[voucherTypeId] = duration;
+        emit VoucherDurationSet(voucherTypeId, duration);
+    }
+
+    function setAssetEligibility(
+        uint256 voucherTypeId,
+        address asset,
+        bool isEligible
+    ) public onlyOwner {
+        VoucherHubStorage storage $ = _getVoucherHubStorage();
+        $._isAssetEligibleToMatchOrdersSponsoringByVoucherTypeId[voucherTypeId][asset] = isEligible;
+        emit AssetEligibilitySet(voucherTypeId, asset, isEligible);
     }
 
     function getIexecAddress() public view returns (address) {
