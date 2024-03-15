e86ccd9693370ddc6ce6357675730325c5bc036d
Gabriel Fournier <gabriel.fournier@iex.ec>
2024-03-15 09:33:50 +0100

    add envent related to voucher type, duration and elegible asset

--- contracts/IVoucherHub.sol
+++ contracts/IVoucherHub.sol
@@ -5,4 +5,7 @@ pragma solidity ^0.8.20;
 
 interface IVoucherHub {
     event VoucherCreated();
+    event VoucherTypeAdded(uint256 indexed voucherTypeId, string description);
+    event VoucherDurationSet(uint256 indexed voucherTypeId, uint256 duration);
+    event AssetEligibilitySet(uint256 indexed voucherTypeId, address asset, bool isEligible);
 }
