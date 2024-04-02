aa4909cbf926d7774fccb8749957cdb34e943cb2
Zied <26070035+zguesmi@users.noreply.github.com>
2024-04-02 16:35:39 +0200

    Add test files

---
+++ trash/abc.sh
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2024 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
+# SPDX-License-Identifier: Apache-2.0
+
+
+sdflj
+sdlfj
---
+++ trash/file1.ts
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: 2024 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
+// SPDX-License-Identifier: Apache-2.0
+
+line1;
+line2;
