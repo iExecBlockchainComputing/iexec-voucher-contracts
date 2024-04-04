2c764ec4818747a2d2b25faa48d5edba715236ad
Gabriel Fournier <gabriel.fournier@iex.ec>
2024-04-04 12:15:11 +0200

    voucher 2 should have duration1

--- test/VoucherHub.test.ts
+++ test/VoucherHub.test.ts
@@ -308,7 +308,7 @@ describe('VoucherHub', function () {
             const createVoucherTx2 = await voucherHub.createVoucher(voucherOwner2, voucherType1);
             const createVoucherReceipt2 = await createVoucherTx2.wait();
             const expectedExpirationVoucher2 = await commonUtils.getExpectedExpiration(
-                duration,
+                duration1,
                 createVoucherReceipt2,
             );
             const voucherAddress2 = await voucherHub.getVoucher(voucherOwner2);
