92c657ebae5642038ba6cbddf9c0198e502a74a0
Gabriel Fournier <gabriel.fournier@iex.ec>
2024-04-10 17:01:23 +0200

    update script on postinstall

--- modify-api-js.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-const apiJsPath = path.join(__dirname, './node_modules/solidity-coverage/lib/api.js');
-let apiJs = fs.readFileSync(apiJsPath, 'utf8');
-
-apiJs = apiJs.replace(/gasPrice \= 0x01/, 'gasPrice = 0x00');
-
-fs.writeFileSync(apiJsPath, apiJs);
