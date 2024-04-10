430755cc78918e8c4cc8e2898f46ebdb98d8cd7e
Gabriel Fournier <gabriel.fournier@iex.ec>
2024-04-10 18:34:41 +0200

    update script on replace

--- scripts/modify-solidity-coverage-lib-api-js.ts
+++ scripts/modify-solidity-coverage-lib-api-js.ts
@@ -10,6 +10,6 @@ import path from 'path';
 const apiJsPath = path.join(__dirname, '../node_modules/solidity-coverage/lib/api.js');
 let apiJs = fs.readFileSync(apiJsPath, 'utf8');
 
-apiJs = apiJs.replace(/gasPrice \= 0x01/, 'gasPrice = 0x00');
+apiJs = apiJs.replace('gasPrice = 0x01', 'gasPrice = 0x00');
 
 fs.writeFileSync(apiJsPath, apiJs);
