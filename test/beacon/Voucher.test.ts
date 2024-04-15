e07817d02ca82dd0ff1796e96aa5f7f9bda5ef9d
Jérémy James Toussaint <33313130+jeremyjams@users.noreply.github.com>
2024-04-15 17:41:10 +0200

    Update test to check match event

--- test/beacon/Voucher.test.ts
+++ test/beacon/Voucher.test.ts
@@ -348,7 +348,7 @@ describe('Voucher', function () {
                 ),
             ).to.be.equal(dealId);
             await expect(voucher.matchOrders(appOrder, datasetOrder, workerpoolOrder, requestOrder))
-                .to.emit(voucher, 'OrdersVoucherMatched')
+                .to.emit(voucher, 'OrdersMatchedWithVoucher')
                 .withArgs(dealId);
             expect(await voucher.getBalance())
                 .to.be.equal(voucherInitialCreditBalance - dealPrice)
@@ -376,7 +376,7 @@ describe('Voucher', function () {
             await expect(voucher.matchOrders(appOrder, datasetOrder, workerpoolOrder, requestOrder))
                 .to.emit(iexecPocoInstance, 'Transfer')
                 .withArgs(requester.address, await voucher.getAddress(), dealPrice)
-                .to.emit(voucher, 'OrdersVoucherMatched');
+                .to.emit(voucher, 'OrdersMatchedWithVoucher');
             expect(await voucher.getBalance())
                 .to.be.equal(voucherInitialCreditBalance)
                 .to.be.equal(await getVoucherBalanceOnIexecPoco())
