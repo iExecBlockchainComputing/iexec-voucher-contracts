2c95702fd071e89f9e8489cd0336b0e7ada870d3
Gabriel Fournier <gabriel.fournier@iex.ec>
2024-05-29 16:07:02 +0200

    test add slither to voucher Hub file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -42,6 +42,7 @@ const config: HardhatUserConfig = {
     },
     dependencyCompiler: {
         paths: ['@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol'],
+        keep: true,
     },
 };
 
