22c9a7a57358844983a94d5d9386ccd219ca7f4f
Le-Caignec <robinlecaignec@yahoo.fr>
2024-12-26 15:49:04 +0100

    fix .env variable

--- env.ts
+++ env.ts
@@ -2,7 +2,10 @@ import 'dotenv/config';
 import { z } from 'zod';
 
 const envSchema = z.object({
-    IEXEC_VOUCHER_HUB_ADDRESS: z.string().regex(/^0x[a-fA-F0-9]{40}$/, 'Invalid Ethereum address'),
+    IEXEC_VOUCHER_HUB_ADDRESS: z
+        .string()
+        .regex(/^0x[a-fA-F0-9]{40}$/, 'Invalid Ethereum address')
+        .optional(),
     IEXEC_VOUCHER_MANAGER_ACCOUNT_INDEX: z
         .string()
         .optional()
