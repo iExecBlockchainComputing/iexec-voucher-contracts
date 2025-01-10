7ad4d66f82605d2edcc335110638f4bcf83fb924
Le-Caignec <robinlecaignec@yahoo.fr>
2025-01-10 11:21:39 +0100

    Add SPDX license identifiers to upgradeHub script

--- scripts/upgradeHub.ts
+++ scripts/upgradeHub.ts
@@ -1,3 +1,5 @@
+// SPDX-FileCopyrightText: 2024 IEXEC BLOCKCHAIN TECH <contact@iex.ec>
+// SPDX-License-Identifier: Apache-2.0
 import { ethers, upgrades } from 'hardhat';
 import { env } from '../config/env';
 import { getDeploymentConfig } from '../deploy/deploy';
