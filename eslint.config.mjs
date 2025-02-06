47086f3304db17e726eedc7d8dfe9405397cfad1
Le-Caignec <robinlecaignec@yahoo.fr>
2025-02-06 15:47:33 +0100

    fix import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,7 @@ export default [
             },
         },
         plugins: {
-            '@typescript-eslint': tseslint,
+            '@typescript-eslint': tsEslint,
             import: importPlugin,
         },
     },
